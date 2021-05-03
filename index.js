c27ca16c57266b3e9d51846d506f70abdee51077
yoshihollez <36663359+yoshihollez@users.noreply.github.com>
2021-05-03 16:44:16 +0200

    updated ytdl core and added ignore for timer warning

--- index.js
+++ index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './routes/App';
 import {name as appName} from './app.json';
-
+import {LogBox} from 'react-native';
+LogBox.ignoreLogs(['Setting a timer']);
 AppRegistry.registerComponent(appName, () => App);
