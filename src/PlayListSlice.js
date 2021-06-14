443fceb566c2c93a142b3802672c247b9a2755ae
yoshihollez <36663359+yoshihollez@users.noreply.github.com>
2021-06-14 14:07:28 +0200

    added export for reducers

--- src/PlayListSlice.js
+++ src/PlayListSlice.js
@@ -3,16 +3,28 @@ import {createSlice} from '@reduxjs/toolkit';
 export const PlayListSlice = createSlice({
   name: 'playList',
   initialState: {
-    value: [],
+    playList: [],
+    playListName: 'PlayList',
+    playListsNames: [{label: 'test', value: 'PlayList'}],
   },
   reducers: {
     setPlayList: (state, action) => {
-      state.value = action.payload;
+      state.playList = action.payload;
+    },
+    setPlayListName: (state, action) => {
+      state.playListName = action.payload;
+    },
+    setPlayListsNames: (state, action) => {
+      state.playListsNames = action.payload;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const {setPlayList} = PlayListSlice.actions;
+export const {
+  setPlayList,
+  setPlayListName,
+  setPlayListsNames,
+} = PlayListSlice.actions;
 
 export default PlayListSlice.reducer;
